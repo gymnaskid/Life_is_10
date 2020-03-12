666b9b64bce63b983db575da8303735d67784e1d
Dylan Anderson <gymnaskid@gmail.com>
2020-03-12 16:51:00 -0600

    added a cut scene to end the 6am-level and transitoin into level 7am_level2 which needs to be built - Dylan

--- Life_is_10/Assets/Scripts/PlayerController.cs
+++ Life_is_10/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed; //how fast can the player move
     public float jumpHeight; //How high we want to jump
+    public bool canControl;
 
     private Rigidbody2D myBody; //the rigid body that will be used to move the player
     
     private Vector2 moveInput; //the direction the player is moving in
     private bool isMoving; //whether the player is moving or not
     private bool isJumping;
+    private bool wallJump;
  
 
     // Start is called before the first frame update
@@ -23,28 +25,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-        
-        //only using x axis because you can only move left/right 
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0).normalized;
-        if(moveInput != Vector2.zero)
+        if (canControl)
         {
-            myBody.velocity = new Vector2(moveInput.x * moveSpeed, myBody.velocity.y);
-        }
-        else //if no left/right inputs stop the horizontal movement, but keep any vertical movement
-        {
-            myBody.velocity = new Vector2(0.0f, myBody.velocity.y);
-            isMoving = false;
-        }
+            //only using x axis because you can only move left/right 
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0).normalized;
+            if (moveInput != Vector2.zero)
+            {
+                myBody.velocity = new Vector2(moveInput.x * moveSpeed, myBody.velocity.y);
+            }
+            else //if no left/right inputs stop the horizontal movement, but keep any vertical movement
+            {
+                myBody.velocity = new Vector2(0.0f, myBody.velocity.y);
+                isMoving = false;
+            }
 
-        //if the player presses space, we want to jump
-        if (!isJumping &&  (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("btnX")))
-        {
-            myBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
-            isJumping = true; //set is jumping to true as this may alter how the player can move or what they can do in the future
-           
+            //if the player presses space, we want to jump
+            if (!isJumping && (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("btnX")))
+            {
+                if (wallJump)
+                {
+                    myBody.AddForce(Vector2.up * jumpHeight / 2, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    myBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                }
+                isJumping = true; //set is jumping to true as this may alter how the player can move or what they can do in the future
+            }
         }
-        
     }
 
 
@@ -56,10 +64,12 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "Floor")
         {
             isJumping = false;
+            wallJump = false;
         }
         if (other.gameObject.tag == "Wall")
         {
             isJumping = false;
+            wallJump = true;
         }
     }
 }
