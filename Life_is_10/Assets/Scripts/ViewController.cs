f6461ff535d86c2076d1779c623356bd6ffe8453
Dylan Anderson <gymnaskid@gmail.com>
2020-03-13 09:58:27 -0600

    switching views now also switches which player you see, and camera focuses on the appropriate one - Dylan

--- Life_is_10/Assets/Scripts/ViewController.cs
+++ Life_is_10/Assets/Scripts/ViewController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ViewController : MonoBehaviour
 {
     public GameObject youngView; //reference to the young view
     public GameObject oldView;//refence to the old view
+
+    public GameObject youngPlayer;
+    public GameObject oldPlayer;
+
+    public CinemachineVirtualCamera cam;
 
     public bool isOldView; //wheter we are currently looking at the old view
 
@@ -16,10 +22,14 @@ public class ViewController : MonoBehaviour
         if(isOldView)
         {
             youngView.SetActive(false);
+            youngPlayer.SetActive(false);
+            cam.Follow = oldPlayer.transform; //make sure camera is following teh right player
         }
         else
         {
             oldView.SetActive(false);
+            oldPlayer.SetActive(false);
+            cam.Follow = youngPlayer.transform;
         }
     }
 
@@ -32,16 +42,29 @@ public class ViewController : MonoBehaviour
             {
                 //turn on the young view
                 youngView.SetActive(true);
-                //turn off the old view
+                //Set the young player to where the old player is
+                youngPlayer.transform.position = new Vector3(oldPlayer.transform.position.x, oldPlayer.transform.position.y, youngPlayer.transform.position.z);
+                youngPlayer.SetActive(true);
+                cam.Follow = youngPlayer.transform;
+                //turn off the old view and old player
                 oldView.SetActive(false);
+                oldPlayer.SetActive(false);
+
                 isOldView = false;
             }
             else
             {
                 //turn on old view
                 oldView.SetActive(true);
+
+                oldPlayer.transform.position = new Vector3(youngPlayer.transform.position.x, youngPlayer.transform.position.y, oldPlayer.transform.position.z);
+                oldPlayer.SetActive(true);
+
                 //turn off youngview
                 youngView.SetActive(false);
+                youngPlayer.SetActive(false);
+                cam.Follow = oldPlayer.transform;
+
                 isOldView = true;
             }
 
